bd1384fe35479724053dc028837e335125b4e8f4
mchscott <michael@shotgundriver.com>
2017-07-13 23:21:06 +1000

    ball launcher

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private Rigidbody2D rigi;
+    private bool launched = false;
+	// Use this for initialization
+	void Start () {
+        rigi = GetComponent<Rigidbody2D>();
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( launched == false )
+        {
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+        }
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Mouse clicked");
+            launched = true;
+            //this.GetComponent.velocity = new Vector2(2f, 10f);
+            //this.rigidbody2D.velocity = new Vector2(2f, 10f);
+            rigi.velocity = new Vector2(3f, 14f);
+        }
+	}
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -18,17 +18,22 @@ public class Paddle : MonoBehaviour {
         float mousePosXRatioOfScreen = Input.mousePosition.x / Screen.width;
         float paddlePosInBlocks = (this.transform.position.x - 82) / 15 * 16;
         // if (  )
-        Debug.Log("paddle pos = " + ( this.transform.position.x - 82) /15 * 16 );
+        //Debug.Log("paddle pos = " + ( this.transform.position.x - 82) /15 * 16 );
         Vector3 paddPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
         mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 
+        /*
         if ( mousePosInBlocks < paddlePosInBlocks )
         {
-            paddPos = new Vector3(this.transform.position.x - 0.1f, this.transform.position.y, this.transform.position.z);
+            paddPos = new Vector3( Mathf.Clamp(this.transform.position.x - 0.1f,82f,97f), this.transform.position.y, this.transform.position.z);
         } else if (mousePosInBlocks > paddlePosInBlocks )
         {
-            paddPos = new Vector3(this.transform.position.x + 0.1f, this.transform.position.y, this.transform.position.z);
+            paddPos = new Vector3(Mathf.Clamp( this.transform.position.x + 0.1f,82f, 97f), this.transform.position.y, this.transform.position.z);
         }
+        */
+
+
+        paddPos = new Vector3(Mathf.Clamp(mousePosInBlocks+82f, 82f, 97f), this.transform.position.y, this.transform.position.z);
         //Debug.Log(mousePosInBlocks);
         //        Debug.Log(Input.mousePosition.x / Screen.width);
         //  Debug.Log("Mouse Moved");
